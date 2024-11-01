02ff309dab3460038224df48d9604a96f197aec2
Iurii Shchegolev <149073874+yashchegolev@users.noreply.github.com>
2024-11-01 19:31:50 +0700

    Update 2.1.3.py

--- 2/2.1/2.1.3.py
+++ 2/2.1/2.1.3.py
@@ -1,1 +1,10 @@
-
+def is_valid(string):
+    if 4 <= len(string) <= 6:
+        return all(map(lambda num: True if num in '0123456789' else False, string))
+    else:
+        return False
+        
+print(is_valid('92134'))
+print(is_valid('89abc1'))
+print(is_valid('900876'))
+print(is_valid('49 83'))
