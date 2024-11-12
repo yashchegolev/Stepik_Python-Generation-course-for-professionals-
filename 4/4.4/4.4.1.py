b891052456e2c963f8dd3068e52fd2cec15357e8
Iurii Shchegolev <149073874+yashchegolev@users.noreply.github.com>
2024-11-12 19:13:31 +0700

    Create 4.4.1.py

---
+++ 4/4.4/4.4.1.py
@@ -0,0 +1,10 @@
+import json
+
+countries = {'Monaco': 'Monaco', 'Iceland': 'Reykjavik', 'Kenya': 'Nairobi', 'Kazakhstan': 'Nur-Sultan',
+             'Mali': 'Bamako', 'Colombia': 'Bogota', 'Finland': 'Helsinki', 'Costa Rica': 'San Jose',
+             'Cuba': 'Havana', 'France': 'Paris', 'Gabon': 'Libreville', 'Liberia': 'Monrovia',
+             'Angola': 'Luanda', 'India': 'New Delhi', 'Canada': 'Ottawa', 'Australia': 'Canberra'}
+
+json_countries = json.dumps(countries, indent=3, sort_keys=True, separators=(',', ' - '))
+
+print(json_countries)
