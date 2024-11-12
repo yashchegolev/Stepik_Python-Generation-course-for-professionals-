29820a0ca2986e6da6f5a9f6cc5c993687680f49
Iurii Shchegolev <149073874+yashchegolev@users.noreply.github.com>
2024-11-12 19:14:20 +0700

    Create 4.4.3.py

---
+++ 4/4.4/4.4.3.py
@@ -0,0 +1,14 @@
+import json
+
+club1 = {"name": "FC Byern Munchen", "country": "Germany", "founded": 1900,
+         "trainer": "Julian Nagelsmann", "goalkeeper": "M. Neuer", "league_position": 1}
+
+club2 = {"name": "FC Barcelona", "country": "Spain", "founded": 1899,
+         "trainer": "Xavier Creus", "goalkeeper": "M. Ter Stegen", "league_position": 7}
+
+club3 = {"name": "FC Manchester United", "country": "England", "founded": 1878,
+         "trainer": "Michael Carrick", "goalkeeper": "D. De Gea", "league_position": 8}
+
+club = [club1, club2, club3]
+with open('4.4.3_data.json', 'w') as file:
+    json.dump(club, file, indent='   ')
