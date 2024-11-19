6fcbbee36742c86b02bf500256b18a7ce84a8a28
Iurii Shchegolev <149073874+yashchegolev@users.noreply.github.com>
2024-11-20 05:17:38 +0700

    Create 6.5.1.py

---
+++ 6/6.5/6.5.1.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+data = [('Books', 1343), ('Books', 1166), ('Merch', 616), ('Courses', 966), ('Merch', 1145), ('Courses', 1061), ('Books', 848), ('Courses', 964), ('Tutorials', 832), ('Merch', 642), ('Books', 815), ('Tutorials', 1041), ('Books', 1218), ('Tutorials', 880), ('Books', 1003), ('Merch', 951), ('Books', 920), ('Merch', 729), ('Tutorials', 977), ('Books', 656)]
+
+result = defaultdict(int)
+
+for dat in data:
+    result[dat[0]] += dat[1]
+'''
+print(result)
+'''
+for key, value in sorted(result.items()):
+    print(f'{key}: ${value}')
