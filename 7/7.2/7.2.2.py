4ebdb2fc19df426b7f17c946496b9fbd2dc7ee8d
Iurii Shchegolev <149073874+yashchegolev@users.noreply.github.com>
2024-11-26 03:48:56 +0700

    Create 7.2.2.py

---
+++ 7/7.2/7.2.2.py
@@ -0,0 +1,9 @@
+food = ['chocolate', 'chicken', 'corn', 'sandwich', 'soup', 'potatoes', 'beef', 'lox', 'lemonade']
+fifth = []
+
+for x in food:
+    try:
+        fifth.append(x[4])
+    except:
+        fifth.append('_')
+print(fifth)
