0d3f153f8cf0932cd09248d3d88cc157394eb538
Iurii Shchegolev <149073874+yashchegolev@users.noreply.github.com>
2024-12-04 04:44:04 +0700

    Create 9.1.9.py

---
+++ 9/9.1/9.1.9.py
@@ -0,0 +1,8 @@
+names = ['Moana', 'Cars', 'Zootopia', 'Ratatouille', 'Coco', 'Inside Out', 'Finding Nemo', 'Frozen']
+budgets = [150000000, 120000000, 150000000, 150000000, 180000000, 175000000, 94000000, 150000000]
+box_offices = [643331111, 462216280, 1023784195, 620702951, 807082196, 857611174, 940335536, 1280802282]
+
+profit = [box_offices[i] - budgets[i] for i in range(len(box_offices))]
+
+for cartoon in sorted(zip(names, profit)):
+    print(f'{cartoon[0]}: {cartoon[1]}$')
